159952314c397767a7bc7e0abf637f8da5f0e677
Greenchik <green@ukr.net>
2019-10-28 14:45:42 +0200

    BeBeBe

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>LaLaLa</p>
+        <p>BeBeBe</p>
         <a
           className="App-link"
           href="https://reactjs.org"
